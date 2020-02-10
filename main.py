e6ece7abdb3a49f79ce4d173373c5150f68acd38
tomoki <tomoki@ai.cs.ehime-u.ac.jp>
2020-02-10 09:03:37 +0900

    logger

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim
 import configparser
+from logging import getLogger, StreamHandler, Formatter, FileHandler, DEBUG
+from utils.logger import get_logger
 
 def main():
 	config = configparser.ConfigParser()
@@ -11,6 +13,11 @@ def main():
 	batchsize = defaults.get('--batchsize')
 	data_dir = defaults.get('--data_dir')
 	output_dir = defaults.get('--out')
+
+	logger = get_logger(output_dir)
+	logger.debug('batchsize : {}'.format(batchsize))
+	logger.debug('data_dir : {}'.format(data_dir))
+	logger.debug('output_dir : {}'.format(output_dir))
 
 if __name__ == '__main__':
 	main()
